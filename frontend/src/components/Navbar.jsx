79bf90b57c428be5b7ac444ac364879fff096d36
RaymanLegends <bbrayman56@gmail.com>
2026-08-21 00:48:15 -0600

    spelling error

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <div className="flex items-center gap-4">
           <Link to={"/create"} className="btn btn-primary rounded-full">
             <PlusIcon className="size-5"/>
-            <span>New Note</span>
+            <span>New Application</span>
           </Link>
         </div>
       </div>
